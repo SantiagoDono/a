227be34c57e570979fc6be1a65017af00cc290f6
santiago dono <donosantiago@gmail.com>
2021-10-20 17:33:54 -0300

    arrancandonga

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,16 +6,72 @@ namespace ConsoleApp1
         // muestro matriz, recorro
         static public void MuestroMatriz(string[,] Matriz)
         {
-           
-        }
-        // agregar frases 
-
-        
-        
-            static public void agregarFrases(string[,] Matriz)
+            for (int indiceP = 0; indiceP < Matriz.GetLength(0); indiceP++)
             {
-            string[,] palabras = new string[25, 10];
+                for (int indiceF = 0; indiceF < Matriz.GetLength(1); indiceF++)
+                {
+                    Console.Write(Matriz[indiceP, indiceF] + " ");
+                }
+                Console.WriteLine();
             }
+        }
+        // NO SE SI ESO ESTA BIEN
+       
+        // MENU
+        public static void menu ()
+        {
+            Console.Clear();
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("MENU");
+            Console.WriteLine("************************************************** \n");
+
+            Console.WriteLine("1- Agregar frase");
+
+            Console.WriteLine("2- Invertir frase");
+
+            Console.WriteLine("3- Mostrar todas las frases");
+
+            Console.WriteLine("4- Buscar una palabra ");
+
+            Console.WriteLine("5- Salir \n"); // no lo pide pero en la clase siempre lo pone asi que por las dudas lo puse 
+
+            Console.Write("ingrese su opcion: ");
+        }
+        public static bool procesoMenu(string Opcion)
+        {
+            switch (Opcion.Trim())
+            {
+                case "1":
+                    {
+                        //agregarFrase();
+                        break;
+                    }
+                case "2":
+                    {
+                        //"invertir frase"
+                        break;
+                    }
+                case "3":
+                    {
+                        MuestroMatriz() ; // aca seria desplegar todo lo que se escribio es solo recorrer la matriz,  
+                        break;
+                    }
+                case "4":
+                    {
+                        // buscar una palabra, hay un ejemplo de como hacerlo  en el ejercicio que hicimos ayer
+                        break;
+                    }
+                case "5": return (false);
+
+                default:
+                    Console.WriteLine("opcion de menu invalida");
+                    Console.ReadLine();
+                    break;
+            }
+            return (true); // para que vuelva a repetirse el menu
+        }
+
+
         
 
            
