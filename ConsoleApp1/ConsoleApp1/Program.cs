387b7db6bac8531924badf56274af3d28dd59348
santiago dono <donosantiago@gmail.com>
2021-10-19 22:02:46 -0300

    joaco

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,38 +3,67 @@
 namespace ConsoleApp1
 {
     class Program {
+        // muestro matriz, recorro
+        static public void MuestroMatriz(string[,] Matriz)
+        {
+           
+        }
+        // agregar frases 
 
         
         
-     
+            static public void agregarFrases(string[,] Matriz)
+            {
+            string[,] palabras = new string[25, 10];
+            }
+        
+
+           
         
         static void Main(string[] args)
         {
             // creacion de la matriz
             string[,] frases = new string[25, 10];
-          
+           
 
+                
+                 
+           
+            
+        }
 
-            try
+        //agregar frase
+        public static void agregarFrase(string[,] frases, int cantFrases)
+        {
+            Console.Clear();
+            Console.WriteLine("Opcion 1: agregar frase");
+            Console.WriteLine();
+            if (frases.GetLength(0) >= cantFrases)
             {
-                for (int fila = 0; fila <= 9; fila++)
+                for (int i1 = 0; i1 < 1; i1 += 0)
                 {
-                    Console.Write("\t" + frases[0, fila]);
-                    Console.WriteLine("ingrese palabra ");
-
-
-                    for (int columna = 0; columna <= 24; columna++)
+                    try
                     {
-
+                        for (int i = 0; i < frases.GetLength(1); i++)
+                        {
+                            Console.Write("Ingrese " + i++ + "º palabra: ");
+                            frases[cantFrases, i] = Console.ReadLine();
+                        }
+                        cantFrases++;
+                        i1++;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Error, ingrese solamente palabras");
                     }
                 }
-            }
-            catch 
-            {
 
-                Console.WriteLine("esta todo bien");
-            }  
-            
+            }
+            else
+            {
+                Console.WriteLine("No queda mas espacio para otras frases.");
+                Console.ReadLine();
+            }
         }
     }
 }
